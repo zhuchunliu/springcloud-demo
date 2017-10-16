00ffe40216aae68afcbe6de5dd5ca22af6d2c0d8
zhuchunliu <296898883@qq.com>
2017-10-16 16:08:25 +0800

    springcloud集config和eureka集群

--- config-server/src/main/java/com/spring/cloud/start/ConfigServerStart.java
+++ config-server/src/main/java/com/spring/cloud/start/ConfigServerStart.java
@@ -3,9 +3,11 @@ package com.spring.cloud.start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerStart {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerStart.class);
