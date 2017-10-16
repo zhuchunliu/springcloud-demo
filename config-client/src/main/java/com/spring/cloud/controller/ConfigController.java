5fa0e531dbc68196fce09fb44a6be23efa4b3712
zhuchunliu <296898883@qq.com>
2017-10-16 14:22:20 +0800

    config配置

--- config-client/src/main/java/com/spring/cloud/controller/ConfigController.java
+++ config-client/src/main/java/com/spring/cloud/controller/ConfigController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/config")
 public class ConfigController {
 
-    @Value("${foo}")
-    String foo;
+    @Value("${demo}")
+    String demo;
 
-    @RequestMapping(value = "/foo")
+    @RequestMapping(value = "/demo")
     public String foo(){
-        return foo;
+        return demo;
     }
 }
--- config-client/src/main/java/com/spring/cloud/start/ConfigClientStart.java
+++ config-client/src/main/java/com/spring/cloud/start/ConfigClientStart.java
@@ -1,13 +1,20 @@
 package com.spring.cloud.start;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.spring.cloud.controller")
 public class ConfigClientStart {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientStart.class);
+    }
+
+    @Autowired
+    void setEnvironment(Environment env) {
+        System.out.println("demo from master: " + env.getProperty("demo"));
     }
 }
