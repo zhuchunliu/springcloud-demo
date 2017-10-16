ab56f19627b5c24ff49c705a7ed46b97f3aa3b63
zhuchunliu <296898883@qq.com>
2017-10-16 18:04:04 +0800

    zipkin配置

---
+++ zipkin-server/src/main/java/com/spring/cloud/start/ZipkinStart.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinStart.class);
+    }
+}
