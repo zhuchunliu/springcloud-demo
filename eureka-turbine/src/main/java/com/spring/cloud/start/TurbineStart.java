4116fcbc384237dca3942c5e8a87c86597de35b0
zhuchunliu <296898883@qq.com>
2017-10-20 18:00:33 +0800

    turbine

---
+++ eureka-turbine/src/main/java/com/spring/cloud/start/TurbineStart.java
@@ -0,0 +1,10 @@
+package com.spring.cloud.start;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Darren on 2017/10/19 0019
+ **/
+@SpringBootApplication
+public class TurbineStart {
+}
