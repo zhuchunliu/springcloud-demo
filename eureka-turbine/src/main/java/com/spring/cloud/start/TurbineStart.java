4d70e96d96b07af0d15113d181d7ffd57714fe9c
zhuchunliu <296898883@qq.com>
2017-10-23 18:06:36 +0800

    turbine集成

--- eureka-turbine/src/main/java/com/spring/cloud/start/TurbineStart.java
+++ eureka-turbine/src/main/java/com/spring/cloud/start/TurbineStart.java
@@ -1,10 +1,16 @@
 package com.spring.cloud.start;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * Created by Darren on 2017/10/19 0019
  **/
 @SpringBootApplication
+@EnableTurbine
 public class TurbineStart {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineStart.class);
+    }
 }
